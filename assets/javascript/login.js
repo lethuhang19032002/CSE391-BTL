6d48100382c0f8279870f306eae17ce0855fcc55
hangthule <hangsuzita@gmail.com>
2022-06-19 09:08:35 +0700

    them login, goc nhin, danh gia

---
+++ assets/javascript/login.js
@@ -0,0 +1,12 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+function login() {
+  if (username.value.trim() == "admin" && password.value.trim() == "admin") {
+    window.location.href = "login.html";
+    window.location = "admin.html";
+    console.log("A");
+  } else {
+    alert("Tên đăng nhập hoặc mật khẩu sai ! ");
+  }
+}
